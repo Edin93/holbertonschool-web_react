22ab65e1198ba6ecccd7f48fa56fab0689298fc1
Edin93 <il_matador@hotmail.fr>
2021-02-09 22:19:24 +0100

    task 1 updated

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -22,36 +22,31 @@ const studentTwo: Student = {
 const studentsList: Array<Student> = [studentOne, studentTwo];
 const labels: string[] = ['firstName', 'location'];
 
-function callback() {
-	let body: HTMLBodyElement = document.getElementsByTagName('body')[0];
-	let table: HTMLTableElement = document.createElement('table');
-	let tbody: HTMLTableSectionElement = document.createElement('tbody');
-	let thead: HTMLTableSectionElement = document.createElement('thead');
+let body: HTMLBodyElement = document.getElementsByTagName('body')[0];
+let table: HTMLTableElement = document.createElement('table');
+let tbody: HTMLTableSectionElement = document.createElement('tbody');
+let thead: HTMLTableSectionElement = document.createElement('thead');
 
-	table.appendChild(thead);
+table.appendChild(thead);
 
-	// for (let i: number = 0; i < labels.length; i++) {
-		// let th: HTMLTableCellElement = document.createElement('th');
-		// th.appendChild(document.createTextNode(`${labels[i]}`));
-		// thead.appendChild(th);
-	// }
+// for (let i: number = 0; i < labels.length; i++) {
+	// let th: HTMLTableCellElement = document.createElement('th');
+	// th.appendChild(document.createTextNode(`${labels[i]}`));
+	// thead.appendChild(th);
+// }
 
-	thead.innerHTML = '<tr><th>firstName</th><th>location</th></tr>';
+thead.innerHTML = '<tr><th>firstName</th><th>location</th></tr>';
 
-	for (let i :number = 0; i < studentsList.length; i++) {
-		let tr: HTMLTableRowElement = document.createElement('tr');
-		tbody.appendChild(tr);
-		const values: string[] = [studentsList[i].firstName, studentsList[i].location]
+for (let i :number = 0; i < studentsList.length; i++) {
+	let tr: HTMLTableRowElement = document.createElement('tr');
+	tbody.appendChild(tr);
+	const values: string[] = [studentsList[i].firstName, studentsList[i].location]
 
-		for (let j :number = 0; j < values.length; j++) {
-			let td: HTMLTableCellElement = document.createElement('td');
-			td.appendChild(document.createTextNode(`${values[j]}`));
-			tr.appendChild(td);
-		}
+	for (let j :number = 0; j < values.length; j++) {
+		let td: HTMLTableCellElement = document.createElement('td');
+		td.appendChild(document.createTextNode(`${values[j]}`));
+		tr.appendChild(td);
 	}
-	table.appendChild(tbody);
-	body.appendChild(table);
 }
-
-callback();
-
+table.appendChild(tbody);
+body.appendChild(table);
