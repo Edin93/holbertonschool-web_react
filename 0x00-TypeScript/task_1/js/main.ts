09ef94a39f5c1198b9077a6b16f45fb9761ce936
Edin93 <il_matador@hotmail.fr>
2021-02-09 15:30:02 +0100

    task 3

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -10,3 +10,7 @@ interface Teacher {
 interface Directors extends Teacher {
 	numberOfReports: number;
 }
+
+function printTeacher (firstName: string, lastName: string): string {
+	return `${firstName[0]}. ${lastName}`;
+}
