0582d04ab69a6f6ded1ed368e9e6bf050388cb70
Edin93 <il_matador@hotmail.fr>
2021-02-09 14:48:36 +0100

    task 0

--- 0x00-TypeScript/task_0/main.ts
+++ 0x00-TypeScript/task_0/main.ts
@@ -1,3 +1,5 @@
+let $ = require('jquery');
+
 interface Student {
 	firstName: string;
 	lastName: string;
@@ -21,20 +23,41 @@ let studentTwo: Student = {
 
 let studentsList: Student[] = [studentOne, studentTwo];
 
-let body = document.getElementsByTagName('body')[0];
-let table = document.createElement('table');
-let tbody = document.createElement('tbody');
+$(document).ready(() => {
+	let body = document.getElementsByTagName('body')[0];
+	let table = document.createElement('table');
+	let tbody = document.createElement('tbody');
 
-table.appendChild(tbody);
+	table.appendChild(tbody);
 
-for (let i = 0; i < studentsList.length; i++) {
-	let tr = document.createElement('tr');
-	tbody.appendChild(tr);
-	let values: string[] = [studentsList[i].firstName, studentsList[i].location]
+	for (let i = 0; i < studentsList.length; i++) {
+		let tr = document.createElement('tr');
+		tbody.appendChild(tr);
+		let values: string[] = [studentsList[i].firstName, studentsList[i].location]
 
-	for (let j = 0; j < values.length; j++) {
-		let td = document.createElement('td');
-		td.appendChild(document.createTextNode(`${values[j]}`));
-		tr.appendChild(td);
+		for (let j = 0; j < values.length; j++) {
+			let td = document.createElement('td');
+			td.appendChild(document.createTextNode(`${values[j]}`));
+			tr.appendChild(td);
+		}
 	}
-}
+	body.appendChild(table);	
+})
+
+// let body = document.getElementsByTagName('body')[0];
+// let table = document.createElement('table');
+// let tbody = document.createElement('tbody');
+
+// table.appendChild(tbody);
+
+// for (let i = 0; i < studentsList.length; i++) {
+// 	let tr = document.createElement('tr');
+// 	tbody.appendChild(tr);
+// 	let values: string[] = [studentsList[i].firstName, studentsList[i].location]
+
+// 	for (let j = 0; j < values.length; j++) {
+// 		let td = document.createElement('td');
+// 		td.appendChild(document.createTextNode(`${values[j]}`));
+// 		tr.appendChild(td);
+// 	}
+// }
