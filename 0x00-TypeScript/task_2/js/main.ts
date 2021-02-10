bf0b47e30f623f70b4de90972464ed2d45c1d85e
Edin93 <il_matador@hotmail.fr>
2021-02-10 14:36:29 +0100

    task 2

---
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -0,0 +1,52 @@
+interface DirectorInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+	workFromHome(): string;
+	getCoffeeBreak(): string;
+	workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+	workFromHome() {
+		return `Working from home`;
+	}
+
+	getCoffeeBreak() {
+		return `Getting a coffee break`;
+	}
+
+	workDirectorTasks() {
+		return `Getting to director tasks`;
+	}
+}
+
+class Teacher implements TeacherInterface {
+	workFromHome() {
+		return `Cannot work from home`;
+	}
+
+	getCoffeeBreak() {
+		return `Cannot have a break`;
+	}
+
+	workTeacherTasks() {
+		return `Getting to work`;
+	}
+}
+
+function createEmployee(salary: number): Director | Teacher {
+	if (typeof(salary) === 'number' && salary < 500) {
+		return new Teacher();
+	}
+	return new Director();
+}
+
+export {
+	Director,
+	Teacher,
+	createEmployee,
+}
