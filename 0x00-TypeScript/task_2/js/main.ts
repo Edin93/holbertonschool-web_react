d88dff56e16d7eafe40d0ed3bf78b3d77b3ccb31
Edin93 <il_matador@hotmail.fr>
2021-02-10 15:42:39 +0100

    task 7

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -59,13 +59,16 @@ function executeWork(employee: DirectorInterface | TeacherInterface): string {
 
 type Subjects = "Math" | "History";
 
-function teachClass(todayClass: Subjects): string {
+function teachClass(todayClass:Subjects): string {
 	if (todayClass === "Math") {
 		return `Teaching Math`;
 	} else if (todayClass === "History") {
 		return `Teaching History`;
 	}
 }
+
+// console.log(teachClass('Math'));
+// console.log(teachClass('History'));
 
 export {
 	Director,
