52dc152fce2747d0dea0a1830b5b88a754c2bb16
Edin93 <il_matador@hotmail.fr>
2021-02-11 17:16:21 +0100

    update js

--- 0x01-Webpack/task_0/src/index.js
+++ 0x01-Webpack/task_0/src/index.js
@@ -1,5 +1,7 @@
 import $ from "jquery";
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$(function() {
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<p>Copyright - Holberton School</p>');
+});
