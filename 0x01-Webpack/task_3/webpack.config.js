9640b13ae64174ffbc8f2fb70bb27a6db072c1fa
Edin93 <il_matador@hotmail.fr>
2021-02-12 16:29:48 +0100

    change webpack config

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -3,17 +3,9 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
-		header: {
-			import: './modules/header/header.js',
-		},
-		body: {
-			import: './modules/body/body.js',
-			dependOn: 'lodash',
-		},
-		footer: {
-			import: './modules/footer/footer.js',
-		},
-		shared: 'jquery',
+		header:'./modules/header/header.js',
+		body:'./modules/body/body.js',
+		footer:'./modules/footer/footer.js',
 	},
 	output: {
 		path: path.resolve(__dirname, 'public'),
@@ -36,8 +28,92 @@ module.exports = {
 	},
 	devtool: 'inline-source-map',
 	devServer: {
-		contentBase: './dist',
+		contentBase: [
+      path.join(__dirname, 'modules'),
+      path.join(__dirname, 'assets'),
+    ],
 		port: 8564,
 	},
 	mode: 'development',
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const path = require('path');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// module.exports = {
+// 	entry: {
+// 		header: {
+// 			import: './modules/header/header.js',
+// 		},
+// 		body: {
+// 			import: './modules/body/body.js',
+// 			dependOn: 'lodash',
+// 		},
+// 		footer: {
+// 			import: './modules/footer/footer.js',
+// 		},
+// 		shared: 'jquery',
+// 	},
+// 	output: {
+// 		path: path.resolve(__dirname, 'public'),
+// 		filename: '[name].bundle.js',
+// 	},
+// 	plugins: [
+// 		new CleanWebpackPlugin(),
+// 	],
+// 	module: {
+// 		rules: [
+// 			{
+// 				test: /\.css$/i,
+// 				use: ['style-loader', 'css-loader'],
+// 			},
+// 			{
+// 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+// 				type: 'asset/resource',
+// 			},
+// 		],
+// 	},
+// 	devtool: 'inline-source-map',
+// 	devServer: {
+// 		contentBase: [
+//       path.join(__dirname, 'modules'),
+//       path.join(__dirname, 'assets'),
+//     ],
+// 		port: 8564,
+// 	},
+// 	mode: 'development',
+// };
