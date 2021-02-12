c5ede0826e6ec326199569c9a5169abf76bfe6e5
Edin93 <il_matador@hotmail.fr>
2021-02-12 17:11:42 +0100

    task 2, update webpack config

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -1,11 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: {
-		header:'./modules/header/header.js',
-		body:'./modules/body/body.js',
-		footer:'./modules/footer/footer.js',
+		lodash: './node_modules/lodash',
+		jquery: './node_modules/jquery',
+		header: {
+			import: './modules/header/header.js',
+		},
+		body: {
+			import: './modules/body/body.js',
+			dependOn: 'jquery',
+		},
+		footer: {
+			import: './modules/footer/footer.js',
+		},
+		shared: 'lodash',
 	},
 	output: {
 		path: path.resolve(__dirname, 'public'),
@@ -13,6 +24,9 @@ module.exports = {
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard',
+    }),
 	],
 	module: {
 		rules: [
@@ -28,92 +42,8 @@ module.exports = {
 	},
 	devtool: 'inline-source-map',
 	devServer: {
-		contentBase: [
-      path.join(__dirname, 'modules'),
-      path.join(__dirname, 'assets'),
-    ],
+		contentBase: './public',
 		port: 8564,
 	},
 	mode: 'development',
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const path = require('path');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-// module.exports = {
-// 	entry: {
-// 		header: {
-// 			import: './modules/header/header.js',
-// 		},
-// 		body: {
-// 			import: './modules/body/body.js',
-// 			dependOn: 'lodash',
-// 		},
-// 		footer: {
-// 			import: './modules/footer/footer.js',
-// 		},
-// 		shared: 'jquery',
-// 	},
-// 	output: {
-// 		path: path.resolve(__dirname, 'public'),
-// 		filename: '[name].bundle.js',
-// 	},
-// 	plugins: [
-// 		new CleanWebpackPlugin(),
-// 	],
-// 	module: {
-// 		rules: [
-// 			{
-// 				test: /\.css$/i,
-// 				use: ['style-loader', 'css-loader'],
-// 			},
-// 			{
-// 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-// 				type: 'asset/resource',
-// 			},
-// 		],
-// 	},
-// 	devtool: 'inline-source-map',
-// 	devServer: {
-// 		contentBase: [
-//       path.join(__dirname, 'modules'),
-//       path.join(__dirname, 'assets'),
-//     ],
-// 		port: 8564,
-// 	},
-// 	mode: 'development',
-// };
