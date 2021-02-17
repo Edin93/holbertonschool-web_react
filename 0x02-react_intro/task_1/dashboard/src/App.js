addc0e9351c4ef9fc4b3b2b3cb30ebee4ffb8451
Edin93 <il_matador@hotmail.fr>
2021-02-17 16:09:38 +0100

    react intro, task 1

---
+++ 0x02-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import logo from './holberton_logo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+      </div>
+      <div className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ 0x02-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,17 @@
+const getFullYear = () => {
+	let current_year = new Date().getFullYear();
+	return current_year;
+}
+
+const getFooterCopy = (isIndex) => {
+	if (isIndex === true) {
+		return "Holberton School";
+	} else {
+		return "Holberton School main dashboard";
+	}
+}
+
+module.exports = {
+	getFooterCopy,
+	getFullYear,
+};
