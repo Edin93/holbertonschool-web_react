9334bdbbe339b83c10e5301de65a559c1aa2f8d2
Edin93 <il_matador@hotmail.fr>
2021-02-23 18:07:06 +0100

    react props, task 2

---
+++ 0x03-react_props/task_2/dashboard/src/App/App.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Header from '../Header/Header.js';
+import Login from '../Login/Login.js';
+import Footer from '../Footer/Footer.js';
+import Notifications from '../Notifications/Notifications.js';
+
+function App() {
+  return (
+    <Fragment>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
