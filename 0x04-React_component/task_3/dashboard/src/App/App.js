b7675062bc4e96757113057437924e6f37b52bb7
Edin93 <il_matador@hotmail.fr>
2021-03-04 18:52:33 +0100

    bind eventhandler function in all task folders

--- 0x04-React_component/task_3/dashboard/src/App/App.js
+++ 0x04-React_component/task_3/dashboard/src/App/App.js
@@ -11,6 +11,7 @@ import { getLatestNotification } from '../utils/utils';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.ctrlHEventHandler = this.ctrlHEventHandler.bind(this);
   }
 
   ctrlHEventHandler(e) {
