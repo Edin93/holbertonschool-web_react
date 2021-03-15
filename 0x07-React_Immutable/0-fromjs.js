d0025d27977931831ae59851a4bf7b18ff481063
Edin93 <il_matador@hotmail.fr>
2021-03-15 16:57:14 +0100

    update task 0

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,8 +1,10 @@
 const { fromJS } = require('immutable');
 
-function getImmutableObject(object) {
+const getImmutableObject = (object) => {
 	const immutableObj = fromJS(object);
 	return (immutableObj);
 };
 
-export default getImmutableObject;
+module.exports = {
+	getImmutableObject
+};
---
+++ 0x07-React_Immutable/0-main.js
@@ -0,0 +1,12 @@
+const { getImmutableObject } = require("./0-fromjs");
+
+let obj = {
+	fear: true,
+	smell: -1033575916.9145899,
+	wall: false,
+	thing: -914767132
+};
+
+let r = getImmutableObject(obj);
+
+console.log(r);
