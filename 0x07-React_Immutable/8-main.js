003131e0b09d7011063fdd6600e42b57aa0f68d7
Edin93 <il_matador@hotmail.fr>
2021-03-16 18:45:56 +0100

    task 8

--- 0x07-React_Immutable/8-main.js
+++ 0x07-React_Immutable/8-main.js
@@ -10,3 +10,22 @@ const grades = {
 
 console.log('printBestStudents: ');
 printBestStudents(grades);
+
+import { Seq } from 'immutable';
+
+const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export default function printBestStudents(object) {
+  let filteredStudents = Seq(object).filter(grade => grade.score > 70).map(student => {
+    let editedStudent = {
+      ...student,
+      firstName: capitalize(student.firstName),
+      lastName: capitalize(student.lastName),
+    };
+    return editedStudent;
+  }).toJS();
+
+  console.log(filteredStudents);
+}
