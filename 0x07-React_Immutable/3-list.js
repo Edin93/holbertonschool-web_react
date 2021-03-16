991efdff3da1582a16982161ebeec4dcb4a0c169
Edin93 <il_matador@hotmail.fr>
2021-03-16 15:19:05 +0100

    task 3, updated indentation

--- 0x07-React_Immutable/3-list.js
+++ 0x07-React_Immutable/3-list.js
@@ -1,9 +1,9 @@
 import { List } from 'immutable';
 
 export function getListObject(array) {
-	return List(array);
+  return List(array);
 }
 
 export function addElementToList(list, element) {
-	return list.push(element);
+  return list.push(element);
 }
