248000f208760bda37ba30af7744e4f64aea9396
Edin93 <il_matador@hotmail.fr>
2021-03-16 11:48:20 +0100

    update task 2 return value

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,11 +1,5 @@
 import Immutable from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-	if (object === null || object === undefined || Object.keys(object).length === 0) {
-		return (undefined);
-	}
-	if (array === null || array === undefined || array.length === 0) {
-		return (undefined);
-	}
 	return Immutable.getIn(Immutable.fromJS(object), array, undefined);
 };
