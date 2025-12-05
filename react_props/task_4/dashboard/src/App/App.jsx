7c3763a91d01c9936e7f07cd4d3633b3d73b19a1
Houssem <il_matador@hotmail.fr>
2025-12-05 18:38:42 +0100

    Move notificationsList declaration inside App component

--- react_props/task_4/dashboard/src/App/App.jsx
+++ react_props/task_4/dashboard/src/App/App.jsx
@@ -6,7 +6,11 @@ import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
-const notificationsList = [
+
+
+export default function App({ isLoggedIn = false }) {
+
+  const notificationsList = [
   { id:1, type:'default', value:'New course available' },
   { id:2, type:'urgent', value:'New resume available' },
   { id:3, type:'urgent', html:{ __html: getLatestNotification()} }
@@ -17,9 +21,7 @@ const coursesList = [
   { id:2, name:'Webpack', credit:20 },
   { id:3, name:'React', credit:40 }
 ];
-
-export default function App({ isLoggedIn = false }) {
-
+  
   return (
     <>
       <Notifications 
